4cbe04aedb81d4dc5176694d6af153bfadddac8a
火箭快餐 <864994121@qq.com>
2020-03-07 08:34:02 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import goods from 'components/goods/goods'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-
-    }
-  ]
+  linkActiveClass: 'active',
+  routes: [{
+    path: '/',
+    redirect: '/goods'
+  }, {
+    path: '/goods',
+    component: goods
+  }]
 })
