628ba169500366ec020c2f001bd72b33456b4015
火箭快餐 <864994121@qq.com>
2020-05-08 22:59:23 +0800

    no message

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="(item, index) in goods" :key="index" class="menu-item">
           <span class="text">
@@ -10,12 +10,38 @@
         </li>
       </ul>
     </div>
-    <div class="food-wrapper"></div>
+    <div class="food-wrapper" ref="foodWrapper">
+      <ul>
+        <li v-for="(item, index) in goods" class="food-list" ref="foodList" :key="index">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="(food, index) in item.foods" class="food-item border-1px" :key="index">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon" />
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import { getGoods } from 'api/index.js'
+import BScroll from 'better-scroll'
 
 export default {
   props: {
@@ -30,14 +56,20 @@ export default {
   },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
-
     this.getGoods()
+    this.$nextTick(() => {
+      this.initScroll()
+    })
   },
   methods: {
     getGoods() {
       getGoods().then(data => {
         this.goods = data
       })
+    },
+    initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+      this.foodScroll = new BScroll(this.$refs.foodWrapper, {})
     }
   }
 }
@@ -54,7 +86,6 @@ export default {
   width 100%
   overflow hidden
   .menu-wrapper
-    display flex
     flex 0 0 80px
     width 80px
     background #f3f5f7
@@ -64,7 +95,7 @@ export default {
       align-items center
       height 54px
       width 64px
-      margin 0 8px            
+      margin 0 8px
       border-1px(rgba(7, 17, 27, 0.1))
       .icon
         display inline-block
@@ -86,9 +117,57 @@ export default {
       .text
         display table-cell
         width 56px
-        vertical-align middle        
+        vertical-align middle
         font-size 12px
-        line-height 14px        
+        line-height 14px
   .food-wrapper
     flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .desc, .extra
+          line-height 10px
+          font-size 10px
+          color rgb(147, 153, 159)
+        .desc
+          line-height 12px
+          margin-bottom 8px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 14px
+            color rgb(240, 20, 20)
+          .old
+            text-decoration line-through
+            font-size 10px
+            color rgb(147, 153, 159)
 </style>
